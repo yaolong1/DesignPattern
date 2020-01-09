94a50c177c9b4d751f5c226953f43f0a925a7879
yaolong1 <1638538651@qq.com>
2020-01-09 22:02:15 +0800

    外观模式

---
+++ 外观模式/apply-pattern/Test/BModuleApi.java
@@ -0,0 +1,19 @@
+package Test;
+
+/**
+ * @author yaoLong
+ * @date 2020-01-09  21:20
+ */
+public interface BModuleApi {
+
+    //对子系统外部
+    public void b1();
+    //子系统内部使用
+    public void b2();
+    //子系统内部使用
+    public void b3();
+
+
+
+    public void testB();
+}
